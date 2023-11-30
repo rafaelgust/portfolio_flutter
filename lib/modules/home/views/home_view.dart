f45028527545191e5089c3f8b4f3005394b006a5
rafaelgust <rafael.appdevs@gmail.com>
2023-11-30 00:10:13 -0300

    Refactor scroll functionality in HomeView

--- lib/modules/home/views/home_view.dart
+++ lib/modules/home/views/home_view.dart
@@ -13,73 +13,101 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final ScrollController _scrollController = ScrollController();
+  bool _isScrollingDown = false;
+  double _lastScrollOffset = 0.0;
+
+  void _scrollListener() {
+    if (_scrollController.offset > _lastScrollOffset) {
+      setState(() => _isScrollingDown = true);
+    } else {
+      setState(() => _isScrollingDown = false);
+    }
+    _lastScrollOffset = _scrollController.offset;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const HeaderView(),
-          Expanded(
-            child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Column(
+          children: [
+            const HeaderView(),
+            const SizedBox(key: GlobalObjectKey(1)),
+            const AboutMe(key: GlobalObjectKey(0)),
+            Container(
+              key: const GlobalObjectKey(2),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Theme.of(context).colorScheme.primary.withOpacity(0.2),
+                    Colors.transparent,
+                  ],
+                ),
+              ),
+              height: 700,
+              child: const ProjectsView(),
+            ),
+            const SizedBox(
+              key: GlobalObjectKey(3),
+              width: double.infinity,
+              height: 900,
+              child: ContactView(),
+            ),
+            const SizedBox(
+              height: 100,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const SizedBox(key: GlobalObjectKey(1)),
-                  const AboutMe(key: GlobalObjectKey(0)),
-                  Container(
-                    key: const GlobalObjectKey(2),
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Theme.of(context)
-                              .colorScheme
-                              .primary
-                              .withOpacity(0.2),
-                          Colors.transparent,
-                        ],
-                      ),
-                    ),
-                    height: 700,
-                    child: const ProjectsView(),
-                  ),
-                  const SizedBox(
-                    key: GlobalObjectKey(3),
-                    width: double.infinity,
-                    height: 900,
-                    child: ContactView(),
-                  ),
-                  const SizedBox(
-                    height: 100,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Copyright © 2023 @Rafael Gust',
-                          style: TextStyle(
-                            fontFamily: 'RobotoMono',
-                            fontSize: 10,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          'Built with Flutter',
-                          style: TextStyle(
-                            fontFamily: 'RobotoMono',
-                            fontSize: 10,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
+                  Text(
+                    'Copyright © 2023 @Rafael Gust',
+                    style: TextStyle(
+                      fontFamily: 'RobotoMono',
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
+                  Text(
+                    'Built with Flutter',
+                    style: TextStyle(
+                      fontFamily: 'RobotoMono',
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+      floatingActionButton: _isScrollingDown
+          ? FloatingActionButton(
+              onPressed: () {
+                _scrollController.animateTo(0.0,
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.easeInOut);
+              },
+              child: const Icon(Icons.arrow_upward),
+            )
+          : null,
     );
   }
 }
